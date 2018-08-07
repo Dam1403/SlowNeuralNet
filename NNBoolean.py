ea8f95f4a58039d439d5de0978336ee9fbb3ce74
Douglas May <douglas.may@red-inc.us>
2018-08-07 09:36:50 -0400

    Civilized Human Fix
    Used numpy and pycharm like a sentient animal

--- NNBoolean.py
+++ NNBoolean.py
@@ -2,18 +2,18 @@ import ActFuncts
 from NeuralNet import NeuralNet
 
 def main():
-    arr3d = [ [ [.1,.3],\
-                [.2,.4] ]  ]
+    arr3d = [ [.9,.3,.4],\
+              [.2,.8,.2],\
+              [.1,.5,.6]]
 
-                
-    NNinput = [.2,.8]
+
+    NNinput = [.9,.1,.8]
     NN = NeuralNet(2,2,ActFuncts.sigmoid)
     NN.set_weights_arr(arr3d)
 
-
+    print(NNinput)
     print(NN)
     print("Eval Result: " + str(NN.evaluate(NNinput)))
-
 
 
 main()
--- NeuralNet.py
+++ NeuralNet.py
@@ -1,49 +1,32 @@
 from Neuron import Neuron
 from RelationTable import RelationTable
+import numpy
 class NeuralNet:
 #if you can get solve weight = deltaO  you can piecewise node functs within threads
     def __init__(self, layer_count, layer_depth,activation_function):
-        self.true_layer_count = layer_count
         self.layer_count = layer_count
-        self.weight_layer_count = layer_count - 1
-
-
-        #plus 2 for input and output layers
-        self.input_length = layer_depth
-        self.layers = [];
-        for i in range(0,self.layer_count):
-            layer = []
-            for j in range(0,layer_depth):
-                layer.append(Neuron(activation_function))
-            self.layers.append(layer)
-        print(self.layers)
-        self.rel_table = RelationTable(self.layer_count,layer_depth)
+        self.layer_depth = layer_depth
+        self.net = numpy.random.rand(layer_count,layer_depth)
+        self.activation_function = activation_function
+        self.curr_hout = []
+		
 
 
     def __str__(self):
         result = ""
         result += "LayerCount: " + str(self.layer_count) + "\n"
-        result += "LayerDepth: " + str(self.input_length) + "\n"
-        result += "Weights: \n\n" + str(self.rel_table)
+        result += "LayerDepth: " + str(self.layer_depth) + "\n"
+        result += "Weights: \n\n" + str(self.net)
         return result
 
+    def prep_train(self,learning_rate):
+        self.learning_rate = learning_rate
 
 
-    #[layer_index][in_neuron_index][out_nueron_index]
-    def set_weights_rand(self,low_bound,high_bound):
-        self.rel_table.init_weights(low_bound,high_bound)
-    def set_weights_arr(self,arr3d):
-        # +2(IO layers) -1(WeightLayers) error check better
-        if(len(arr3d) != self.weight_layer_count):
-            raise NNWeightLayerException("Weight Array Layer Count Mismatch")
-        if(len(arr3d[0][0]) != self.input_length):
-            raise NNWeightDepthException("Weight Array Layer Depth Mismatch")
-
-        for layer_index in range(0,len(arr3d)):
-            for in_neuron_index in range(0,len(arr3d[0][0])):
-                for out_nueron_index in range(0,len(arr3d[0][0])):
-                    value = arr3d[layer_index][in_neuron_index][out_nueron_index]
-                    self.rel_table.rel_set(layer_index,in_neuron_index,out_nueron_index,value)
+    def set_weights_arr(self,arr2d):
+        self.net = numpy.array(arr2d)
+        self.layer_count = self.net.shape[1]
+        self.layer_depth = self.net.shape[0]
 
 
     def set_weights_file(self,filename):
@@ -54,27 +37,19 @@ class NeuralNet:
                 linearr = line.split(" ")
 
     def evaluate(self, input_arr):
-        self.input_layer_write(input_arr)
-        for layer_index in range(0,self.weight_layer_count):
-            self.IOcrunch_layer(layer_index)
-        return [self.layers[-1][i].value for i in range(0,self.input_length)]
-
+        input_arr = numpy.array(input_arr)
+        hin = numpy.dot(self.net, input_arr)
+        hout = self.activation_function(hin)
+        print("hout: 0" + str(hout))
+        for layer_index in range(1,self.layer_count - 1):
+            hin = numpy.dot(self.net,hout)
+            hout =  self.activation_function(hin)
+            print("hout: {0} ".format(layer_index) + str(hout))
+        return hout
 
 
     def IOcrunch_layer(self, layer_index):
-        print("Layer_index "+str(layer_index))
-        in_layer = self.layers[layer_index]
-        out_layer = self.layers[layer_index + 1]
-        weight = 0;
-
-        for out_neuron_index in range(self.input_length):
-            out_neuron = out_layer[out_neuron_index]
-            new_value = 0
-            for in_neuron_index in range(self.input_length):
-                in_neuron = in_layer[in_neuron_index]
-                weight = self.rel_table.rel_get(layer_index,in_neuron_index,out_neuron_index)
-            new_value += in_neuron.act_funct(in_neuron.value * weight)
-            out_neuron.value = new_value
+        pass
 
 
     def input_layer_write(self,input_arr):
