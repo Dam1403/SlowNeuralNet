19f9f3bfd3f51f315181c45938f1cac23c98a34e
Douglas May <douglas.may@red-inc.us>
2018-08-08 14:29:40 -0400

    IT LIVESSSS!!!
    Backpropogate now

--- NNBoolean.py
+++ NNBoolean.py
@@ -2,14 +2,18 @@ import ActFuncts
 from NeuralNet import NeuralNet
 
 def main():
-    arr3d = [ [.9,.3,.4],\
-              [.2,.8,.2],\
+    layer1 = [ [.9,.3,.4],
+              [.2,.8,.2],
               [.1,.5,.6]]
+
+    layer2 = [ [.3,.7,.5],
+              [.6,.5,.2],
+              [.8,.1,.9]]
 
 
     NNinput = [.9,.1,.8]
-    NN = NeuralNet(2,2,ActFuncts.sigmoid)
-    NN.set_weights_arr(arr3d)
+    NN = NeuralNet(3,3,ActFuncts.sigmoid)
+    NN.set_weight_layers(layer1,layer2)
 
     print(NNinput)
     print(NN)
--- NeuralNet.py
+++ NeuralNet.py
@@ -5,8 +5,9 @@ class NeuralNet:
 #if you can get solve weight = deltaO  you can piecewise node functs within threads
     def __init__(self, layer_count, layer_depth,activation_function):
         self.layer_count = layer_count
+        self.weight_layer_count = layer_count - 1
         self.layer_depth = layer_depth
-        self.net = numpy.random.rand(layer_count,layer_depth)
+        self.net = [None for i in range(0,self.weight_layer_count)]
         self.activation_function = activation_function
         self.curr_hout = []
 		
@@ -23,12 +24,15 @@ class NeuralNet:
         self.learning_rate = learning_rate
 
 
-    def set_weights_arr(self,arr2d):
-        self.net = numpy.array(arr2d)
-        self.layer_count = self.net.shape[1]
-        self.layer_depth = self.net.shape[0]
-
-
+    def set_weight_layers(self,*weight_layers):
+        format_str = "Layer count is {0}, {1} weight 2darrs were provided"
+        if len(weight_layers) != self.weight_layer_count:
+            print(format_str.format(self.weight_layer_count,len(weight_layers)))
+            return
+        new_net = []
+        for i in range(0,len(weight_layers)):
+            new_net.append(numpy.array(weight_layers[i]))
+        self.net = new_net
     def set_weights_file(self,filename):
         with open(filename,"r") as f:
             line = f.readline()
@@ -38,11 +42,11 @@ class NeuralNet:
 
     def evaluate(self, input_arr):
         input_arr = numpy.array(input_arr)
-        hin = numpy.dot(self.net, input_arr)
+        hin = numpy.dot(self.net[0], input_arr)
         hout = self.activation_function(hin)
         print("hout: 0" + str(hout))
-        for layer_index in range(1,self.layer_count - 1):
-            hin = numpy.dot(self.net,hout)
+        for layer_index in range(1,self.weight_layer_count):
+            hin = numpy.dot(self.net[layer_index],hout)
             hout =  self.activation_function(hin)
             print("hout: {0} ".format(layer_index) + str(hout))
         return hout
